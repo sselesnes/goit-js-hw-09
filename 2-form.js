c24cbd6b995db1cca1c14d3eaced766870668283
sselesnes <160606809+sselesnes@users.noreply.github.com>
2025-03-17 05:47:35 +0000

    Deploying to gh-pages from @ sselesnes/goit-js-hw-09@958de94cd6e5e41714f38d9a4b9b0e03ec1b628f 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-ihXV3GyG.js";const a=document.querySelector(".feedback-form");a.addEventListener("input",e=>{const t=e.target.value;e.target.name});a.addEventListener("submit",e=>{e.preventDefault()});
+//# sourceMappingURL=2-form.js.map
