94283badd89ed99394c5578cdaf8961fccb84b02
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-03-17 08:51:16 +0200

    form markup and input localStorage

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,14 +2,14 @@ import '../css/styles.css';
 
 const formData = { email: '', message: '' };
 const formFeedback = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
 
 formFeedback.addEventListener('input', event => {
-  const input = event.target.value;
-  event.target.name === 'email'
-    ? (formData.email = input)
-    : (formData.message = input);
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
 formFeedback.addEventListener('submit', event => {
   event.preventDefault();
+  console.log(localStorage.getItem(localStorageKey));
 });
