958de94cd6e5e41714f38d9a4b9b0e03ec1b628f
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-03-17 07:46:46 +0200

    form markup

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,15 @@
 import '../css/styles.css';
+
+const formData = { email: '', message: '' };
+const formFeedback = document.querySelector('.feedback-form');
+
+formFeedback.addEventListener('input', event => {
+  const input = event.target.value;
+  event.target.name === 'email'
+    ? (formData.email = input)
+    : (formData.message = input);
+});
+
+formFeedback.addEventListener('submit', event => {
+  event.preventDefault();
+});
